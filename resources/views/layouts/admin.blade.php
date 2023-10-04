24a35e626bb73293fce9c961abfc58e886b360bb
valeria caramellino <valeriacaramellino@gmail.com>
2023-10-04 16:45:09 +0200

    aggiunta rotte nella vista admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -43,6 +43,20 @@
                             </li>
 
                             <li class="nav-item">
+                                <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.events.index' ? 'bg-secondary' : '' }}" href="{{route('admin.events.index')}}">
+                                    <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Eventi
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.events.create' ? 'bg-secondary' : '' }}" href="{{route('admin.events.create')}}">
+                                    
+                                    <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Crea Evento
+
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
                                 <a class="nav-link text-white" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                     <i class="fa-solid fa-sign-out-alt fa-lg fa-fw"></i> {{ __('Logout') }}
                                 </a>
