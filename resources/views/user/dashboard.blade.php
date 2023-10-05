47434614416609f7327fd47cbfb4395a66f33f90
valeria caramellino <valeriacaramellino@gmail.com>
2023-10-05 14:39:41 +0200

    style user section

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -2,40 +2,58 @@
 
 @section('content')
 <div class="container-fluid mt-4">
+    {{-- row titolo della sezione --}}
     <div class="row justify-content-center">
         <div class="col-md-8 mx-auto text-center my-2">
-            <h3>Benvenuto {{$user->name}} {{$user->surname}}</h3>
+            <h4>Benvenuto {{$user->name}} {{$user->surname}}</h4>
             <h5>Elenco Eventi a cui sei iscritto </h5>
         </div>
     </div>
-
-    @if(session('success'))
-    <div class="alert alert-success">
-        {{ session('success') }}
-    </div>
-    @endif
-
-    <div class="row justify-content-around">
-        
-    @if ( count($userEvents)  > 0)
     
-        @foreach ($userEvents as $event)
-        <div class="card">
-            <div class="card-header">
-                <p>{{$event->title}}</p>
-            </div>
-            <div class="card-body">
-                <p>{{$event->start_event}}</p>
+    {{-- row successo aggiunta in evento --}}
+    <div class="row">
+        @if(session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+        @endif
+    </div>
+
+</div>
+
+{{-- container con scroll in modo che la pagina rimanga delle dimentioni giuste --}}
+<div class="container-fluid overflow-y-scroll mt-2">
+    
+    {{--se l'utente si è registrato ad eventi li mostra  --}}
+    @if ( count($userEvents)  > 0)
+        
+        <div class="row justify-content-around flex-wrap">
+            
+            @foreach ($userEvents as $event)
+                <div class="card col-md-3 mx-2 my-1">
+
+                    <div class="card-body text-center">
+                        <h5>{{$event->title}}</h5>
+                        <p>{{$event->start_event}}</p>
+                    </div>
+
+                </div>
+            @endforeach
+
+        </div>
+
+    {{--altrimenti mostra un messaggio  --}}
+    @else
+        
+        <div class="row">
+            <div class="col-6 mx-auto text-center">
+                <h5>Non ti sei ancora iscritto a nessun evento</h5>
             </div>
         </div>
-        @endforeach
 
-    @else
-    <h4>non sei ancora registrato a nessun evento</h4>
     @endif
 
-    
-        
-    </div>
+
 </div>
+
 @endsection
--- resources/views/user/events/index.blade.php
+++ resources/views/user/events/index.blade.php
@@ -1,39 +1,57 @@
 @extends('layouts.user')
 
 @section('content')
+
 <div class="container-fluid mt-4">
-    <h5>Elenco di tutti gli Eventi</h5>
-    <div class="row justify-content-around">
+    
+    {{-- row titolo della sezione --}}
+    <div class="row justify-content-center">      
+        <div class="col-auto">
+            <h4>Elenco di tutti gli Eventi</h4>
+        </div>
+    </div>
+    
+    {{-- row insuccesso aggiunta in evento --}}
+    <div class="row">
+            
         @if(session('error'))
             <div class="alert alert-danger">
                 {{ session('error') }}
             </div>
         @endif
+    
+    </div>
+    
+</div>
+
+{{-- container con scroll in modo che la pagina rimanga delle dimentioni vh --}}
+<div class="container-fluid overflow-y-scroll mt-2">
+    
+    <div class="row justify-content-around flex-wrap">
         
+        {{-- elenco eventi --}}
+
         @foreach ($events as $item)
-        <div class="card col-auto">
-            <div class="card-header">
-                <p>{{$item->title}}</p>
-            </div>
-            <div class="card-body">
-                <p>{{$item->start_event}}</p>
-            </div>
-            <div class="card-footer">
-                
-                <form  action="{{ route('user.events.join') }}" method="POST">
-                    @csrf
-                    <input type="hidden" name="event_id" value="{{ $item->id }}">
+            <div class="card col-md-3 mx-2 my-1">
 
-                    <button type="submit" class="btn btn-primary">
-                        JOIN
-                    </button>
-                </form>
+                <div class="card-body text-center">
 
+                    <h5>{{$item->title}}</h5>
+                    <p>{{$item->start_event}}</p>
+
+                    <form  action="{{ route('user.events.join') }}" method="POST">
+                        @csrf
+                        <input type="hidden" name="event_id" value="{{ $item->id }}">
+        
+                        <button type="submit" class="btn btn-primary w-100">
+                            JOIN
+                        </button>
+                    </form>
+
+                </div>
+           
             </div>
-        </div>
         @endforeach
-
-
     
     </div>
 </div>
