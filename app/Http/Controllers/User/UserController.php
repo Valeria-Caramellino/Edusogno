c9883b676062cb327be25244a4fc822e364f4f6e
valeria caramellino <valeriacaramellino@gmail.com>
2023-10-06 12:31:57 +0200

    add edit update user

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -5,7 +5,8 @@ use App\Http\Controllers\Controller;
 use App\Models\Event;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
 class UserController extends Controller
 {
     public function index(){
@@ -16,5 +17,27 @@ class UserController extends Controller
         return view('user.dashboard', compact( 'user', 'userEvents' ));
     }
     
-  
+    public function edit() {
+        $user = Auth::user();
+        return view('user.edit', compact('user'));
+    }
+    
+    public function update(Request $request) {
+        $user = Auth::user();
+    
+        $this->validate($request, [
+            'name' => ['required', 'string', 'max:255'],
+            'surname' => ['required', 'string', 'max:255'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+    
+        $user->update([
+            'name' => $request->input('name'),
+            'surname' => $request->input('surname'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+    
+        return redirect()->route('user.dashboard')->with('success', 'Profilo aggiornato con successo.');
+    }
+    
 }
